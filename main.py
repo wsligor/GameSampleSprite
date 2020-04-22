4ab78a6c6fcf3373c7a01708235349114591d06d
Ligor <fornet11@mail.ru>
2020-04-22 21:09:36 +0300

    begin

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 # Pygame шаблон - скелет для нового проекта Pygame
 import pygame
 import random
-import math
 import os
-import sys
 from settings import Settings
 from pygame.sprite import Sprite
 
@@ -13,13 +11,10 @@ class Mob(Sprite):
         super().__init__()
         self.s = s
         self.id = id
-        #self.image = pygame.image.load(os.path.join(s.img_folder, 'p3_hurt.png'))
-        self.image = pygame.Surface((10, 10))
-        self.image.fill(s.BLACK)
+        self.image = pygame.image.load(os.path.join(s.img_folder, 'p3_hurt.png'))
         self.rect: pygame.Rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        self.size = self.rect.w * self.rect.h
         self.speedx = 3
         self.speedy = 5
 
@@ -31,16 +26,7 @@ class Mob(Sprite):
         self.rect.x += self.speedx
         self.rect.y += self.speedy
 
-    def change_size(self, size):
-        new_size = self.size + size
-        new_size = int(math.sqrt(new_size))
-        self.image = pygame.Surface((new_size, new_size))
-        self.image.fill(self.s.RED)
-        self.rect = self.image.get_rect()
-        self.size = new_size**2
-        pass
-
-def MobCollide(all_s, s):
+def MobCollide(all_s):
     mob: Mob
     for i in all_s:
         first = i
@@ -48,38 +34,10 @@ def MobCollide(all_s, s):
         for j in all_s:
             second = j
             if first.rect.colliderect(second.rect):
-                 first.speedx = -first.speedx
-                 first.speedy = -first.speedy
-                 second.speedx = -second.speedx
-                 second.speedy = -second.speedy
-                 # if first.color == s.RED and second.color == s.BLACK:
-                 #     all_s.remove(second)
-                 #     first.change_size(100)
-                 #     first.update()
-                 #     all_s.add(first)
-                 #     break
-                 # if first.color == s.BLACK and second.color == s.RED:
-                 #     second.change_size(100)
-                 #     second.update()
-                 #     break
-                 # if first.color == s.RED and second.color == s.RED:
-                 #     first.speedx = -first.speedx
-                 #     first.speedy = -first.speedy
-                 #     second.speedx = -second.speedx
-                 #     second.speedy = -second.speedy
-                 #     first.update()
-                 #     second.update()
-                 #     all_s.add(first)
-                 #     break
-                 # if first.color == s.BLACK and second.color == s.BLACK:
-                 #     if random.randint(0, 1) == 0:
-                 #         second.change_size(100)
-                 #         second.update()
-                 #     else:
-                 #         first.change_size(100)
-                 #         first.update()
-                 #         all_s.add(first)
-                 #     break
+                first.speedx = -first.speedx
+                first.speedy = -first.speedy
+                second.speedx = -second.speedx
+                second.speedy = -second.speedy
             first.update()
             second.update()
         all_s.add(first)
@@ -87,7 +45,6 @@ def MobCollide(all_s, s):
 
 def main():
     # Создаем игру и окно
-    if sys.platform in ["win32", "win64"]: os.environ["SDL_VIDEO_CENTERED"] = "1"
     pygame.init()
     pygame.mixer.init()
     s = Settings()
@@ -97,21 +54,20 @@ def main():
     all_spites = pygame.sprite.Group()
     # id Mobs
     id = 1
-    for i in range(100):
+    for i in range(2):
         mob = Mob(s, id, random.randint(0+50, s.WIDTH-50), random.randint(0+50, s.HEIGHT-50))
         all_spites.add(mob)
         id += 1
 
     # Загрузка фона
-    # background = pygame.image.load(os.path.join(s.img_folder, 'cie.png')).convert()
-    # background_rect = background.get_rect()
+    background = pygame.image.load(os.path.join(s.img_folder, 'cie.png')).convert()
+    background_rect = background.get_rect()
 
     # Цикл игры
     running = True
     while running:
         # Держим цикл на правильной скорости
-        #clock.tick(s.FPS)
-        pygame.time.delay(100)
+        clock.tick(s.FPS)
         # Ввод процесса (события)
         for event in pygame.event.get():
             # check for closing window
@@ -120,11 +76,11 @@ def main():
 
         # Обновление
         all_spites.update()
-        MobCollide(all_spites, s)
+        MobCollide(all_spites)
 
         # Рендеринг
-        screen.fill(s.WHITE)
-        #screen.blit(background, background_rect)
+        screen.fill(s.BLACK)
+        screen.blit(background, background_rect)
         all_spites.draw(screen)
         # После отрисовки всего, переворачиваем экран
         pygame.display.flip()
